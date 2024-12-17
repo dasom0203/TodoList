971d09a55d814fde51a3a23c6c6eac266398b1b8
dasom <tlsektha3852@hanmail.net>
2024-12-17 23:50:28 +0900

    todo

--- FRONT/src/components/TodoItem.js
+++ FRONT/src/components/TodoItem.js
@@ -1,10 +1,28 @@
 import React from "react";
 import { Checkbox } from 'antd';
+import axios from "axios";
 
 function TodoItem({ item }) {
 
-    const onChange = (e) => {
-        console.log(`checked = ${e.target.checked}`);
+    // Todo 완료 상태 변경
+    const onChange = (checked) => {
+        // console.log(`checked = ${e.target.checked}`);
+        console.log("todo체크박스 상태 확인!", checked);
+
+        // 상태 값 토글
+        const newStatus = checked ? '완료' : '미완료';
+
+        // [PUT] /todo/status
+        axios.put('/todo/status',{
+            todoNum:item.todoNum,
+            todoStatus:newStatus
+        })
+        .then(function(response){
+            console.log("업데이트 성공!",response);
+        })
+        .catch(function(error){
+            console.log("item 완료 실패!!");
+        })
     };
 
     return (
@@ -12,7 +30,8 @@ function TodoItem({ item }) {
 
         <div className="todo-item">
             <Checkbox
-                onChange={() => onChange(item.todoNum)}
+                // 체크박스의 상태 (true/false)를 가지고 온다.
+                onChange={(e) => onChange(e.target.checked)}
                 checked={item.todoStatus == '완료'}>{item.todoContent}</Checkbox>
             <p>{item.todoImportant ? '중요' : '일반'}</p>
         </div>
